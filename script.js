2798ecd5b021ed08f447a802514452d79c84d5c3
biswash <biswashkhanal10@gmail.com>
2025-05-28 17:28:41 +0545

    episode 6

--- script.js
+++ script.js
@@ -1,135 +1,56 @@
-// function sayhello() {
-//     alert('hello world');
-// }
+// function in java script
+// A function is a like a resuable block of codes that performs specific tasks.
 
-// what is variable
-// 1. var
-// 2. let
-// 3.const
+// syantax
+function functionName() {
+  // block of code.
+}
 
-// 1.var
+// example
+function greet() {
+  console.log("hello, welcome to java script");
+}
+// here greet is the function name
+// when we call it, it will run the code inside the curly braces {}
 
-var number = 123;
+// calling a function
+greet();
 
-// syntax
-// variable variableName = value
+//  tip: for calling a  function means telling it to run.
 
-// 2. let
-let hi = 456;
+//  Topic 1: functions with parametrs
+function greetUser(name) {
+  console.log("hello, " + name + "!");
+  console.log(`hello, ${name}!`);
+}
 
-// 3. const
-const okay = 789;
+greetUser("biswash");
 
-// declare / declaration
-// initialize / intialization
+// the value you pass inside () is called an argument. ( Arguments are used during the call)
+// name here is a parameter. (Parameters are used in the definations)
 
-//declare
-let person;
+// topic 2: functions that returns values
+function add(a, b) {
+  return a + b;
+}
+let result = add(2, 4);
+console.log(result);
 
-//initialize
-person = "biswash";
+// Why use functions ??
+// reusable, easy for debugging, clean and organized code
 
-// let person = "biswash" decleration + initialization
+// mini practise challange
+function area(l, b) {
+  return l * b;
+}
+let answer = area(10, 5);
+console.log(answer);
 
-//const = constant
+// BONUS: Arrow functions {Modern java script}
+// Arrow functions are a shorter way to write functions.
 
-let surname = "khanal";
-surname = "gurung";
-
-// console.log(surname);
-
-// naming rules for java script variables.
-
-// 1. can contain letters, digits, underscore and dollar signs.
-// eg: person, person123, _person and $person.
-
-// 2. must start from letter or  _ or $ but not a from a number.
-// eg: 123person  (not allowed)
-
-// 3. case sensitive (person and Person are different)
-
-// 4. cannot be a reserved keyword like let, class, return etc.
-
-// Data types
-
-// 1. string
-let city = "pokhara";
-let message = "hello, pokhara! ";
-
-// backticks
-let greeting = `good morning ${city}`;
-
-// 2. number
-//integer
-let age = 30;
-//decimal
-let price1 = 99.99;
-
-// 3. Boolean
-let isOnline = false;
-let isDarkmode = true;
-
-// 4. undefined
-let country;
-//  console.log(country)
-
-// 5. null
-let selectedcolor = null;
-
-//  console.log(selectedcolor)
-
-// 6. Object
-let person2 = {
-  name: "ram",
-  age: 30,
-  isstudent: false,
+const sayHi = () => {
+  console.log("Hi");
 };
 
-//  console.log(person2)
-
-// Operators
-
-// 1. arithmetic operators
-let a = 10;
-let b = 20;
-
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b)
-
-// 2. assignment operators
-
-let x = 5;
-x += 5; // x = x+5
-x -= 5; // x = x-5
-x *= 5; // x = x*5
-
-// console.log(x)
-
-// 3. comparison operators
-
-// console.log(5 == "5") // true (loose equality)
-// console.log(5 === "5") // false (strict equality)
-// console.log(5 != 4);   // not equals to
-// console.log(10 > 5);
-// console.log(3 <= 3);  // less than equal to
-
-// Bonus tip
-// == compares values, === compares both values and data types.
-
-// 4. Logical Operators
-let isLoggedIn = true;
-let isAdmin = false;
-
-console.log(isLoggedIn && isAdmin); // AND OPERATORS  (both must be true)
-console.log(isLoggedIn || isAdmin); // or operator  (atleat one must be true)
-console.log(!isLoggedIn); // not (flips the value)
-
-// mini practise challenge
-let amount = 100;
-let tax = 0.1;
-
-let total = amount + amount * tax;
-console.log("total amount = ", total);
+sayHi();
