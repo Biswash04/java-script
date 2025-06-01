155d75dc7278ff9c359ff99302a864d8aad82051
biswash <biswashkhanal10@gmail.com>
2025-06-01 17:34:11 +0545

    loops

--- script.js
+++ script.js
@@ -1,135 +1,41 @@
-// function sayhello() {
-//     alert('hello world');
-// }
+// Topic: what is loop ?
+// A Loop lets us run the same block of code again - until certain condition is met.
 
-// what is variable
-// 1. var
-// 2. let
-// 3.const
+// types of loops in javascript
+// 1. for loop
+// 2. while loop
+// 3. do while loop
 
-// 1.var
+// 1. FOR LOOP
+for (let i = 1; i <= 5; i++) {
+  // console.log("number", i);
+}
 
-var number = 123;
+// break down
+// let i = 1 start from
+// i <= 5 run as long as i is less than or equal to 5
+// i++ increase i by 1 each time
 
-// syntax
-// variable variableName = value
+// 2. WHILE LOOP
+let count = 1;
 
-// 2. let
-let hi = 456;
+while (count <= 3) {
+  // console.log("count", count);
+  count++;
+}
 
-// 3. const
-const okay = 789;
+// 3. DO WHILE LOOP
+let num = 1;
 
-// declare / declaration
-// initialize / intialization
+do {
+  // console.log("num", num);
+  num++;
+} while (num <= 1);
 
-//declare
-let person;
+// even if the condition is false, do while runs at least one time
 
-//initialize
-person = "biswash";
-
-// let person = "biswash" decleration + initialization
-
-//const = constant
-
-let surname = "khanal";
-surname = "gurung";
-
-// console.log(surname);
-
-// naming rules for java script variables.
-
-// 1. can contain letters, digits, underscore and dollar signs.
-// eg: person, person123, _person and $person.
-
-// 2. must start from letter or  _ or $ but not a from a number.
-// eg: 123person  (not allowed)
-
-// 3. case sensitive (person and Person are different)
-
-// 4. cannot be a reserved keyword like let, class, return etc.
-
-// Data types
-
-// 1. string
-let city = "pokhara";
-let message = "hello, pokhara! ";
-
-// backticks
-let greeting = `good morning ${city}`;
-
-// 2. number
-//integer
-let age = 30;
-//decimal
-let price1 = 99.99;
-
-// 3. Boolean
-let isOnline = false;
-let isDarkmode = true;
-
-// 4. undefined
-let country;
-//  console.log(country)
-
-// 5. null
-let selectedcolor = null;
-
-//  console.log(selectedcolor)
-
-// 6. Object
-let person2 = {
-  name: "ram",
-  age: 30,
-  isstudent: false,
-};
-
-//  console.log(person2)
-
-// Operators
-
-// 1. arithmetic operators
-let a = 10;
-let b = 20;
-
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-// console.log(a/b);
-// console.log(a%b)
-
-// 2. assignment operators
-
-let x = 5;
-x += 5; // x = x+5
-x -= 5; // x = x-5
-x *= 5; // x = x*5
-
-// console.log(x)
-
-// 3. comparison operators
-
-// console.log(5 == "5") // true (loose equality)
-// console.log(5 === "5") // false (strict equality)
-// console.log(5 != 4);   // not equals to
-// console.log(10 > 5);
-// console.log(3 <= 3);  // less than equal to
-
-// Bonus tip
-// == compares values, === compares both values and data types.
-
-// 4. Logical Operators
-let isLoggedIn = true;
-let isAdmin = false;
-
-console.log(isLoggedIn && isAdmin); // AND OPERATORS  (both must be true)
-console.log(isLoggedIn || isAdmin); // or operator  (atleat one must be true)
-console.log(!isLoggedIn); // not (flips the value)
-
-// mini practise challenge
-let amount = 100;
-let tax = 0.1;
-
-let total = amount + amount * tax;
-console.log("total amount = ", total);
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log("number", i);
+  }
+}
